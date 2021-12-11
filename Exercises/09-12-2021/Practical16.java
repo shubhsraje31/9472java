efa8358c1ae0303bd9fcdfd8a938d6b2701f3329
shubhsraje31 <95090213+shubhsraje31@users.noreply.github.com>
2021-12-11 18:44:15 +0530

    Create Practical16.java

---
+++ Exercises/09-12-2021/Practical16.java
@@ -0,0 +1,15 @@
+//Write a program in Java to display the first 10 natural numbers.
+
+public class Practical16{
+    
+  public static void main(String[] args)
+    {     
+    int i;
+	System.out.println ("The first 10 natural numbers are:\n");
+	for (i=1;i<=10;i++)
+	{      
+		System.out.println (i);
+	}
+System.out.println ("\n");
+}
+}

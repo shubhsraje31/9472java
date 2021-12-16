91dfda1bec7a8992da5827d7462d92d0204a1d8e
shubhsraje31 <95090213+shubhsraje31@users.noreply.github.com>
2021-12-16 18:36:18 +0530

    Create Practical17.java

---
+++ Exercises/16-12-2021/Practical17.java
@@ -0,0 +1,9 @@
+// Narrow casting
+public class Practical17 {
+ public static void main(String[] args) {
+ double myDouble = 9.78;
+ int myInt = (int) myDouble; 
+ System.out.println(myDouble); 
+ System.out.println(myInt); 
+ }
+}

6ae66154039dbc9d227b2de7e8f4ecc1411ee4ac
shubhsraje31 <95090213+shubhsraje31@users.noreply.github.com>
2021-12-22 17:58:52 +0530

    Add files via upload

---
+++ Exercises/22-12-2021/Program2.java
@@ -0,0 +1,10 @@
+
+public class Program2 {
+
+		public static void main(String[] argv)
+		{
+			char ch = 'c';
+			int num = 88;
+			ch = 's';
+		}
+	}
---
+++ Exercises/22-12-2021/Program3.java
@@ -0,0 +1,8 @@
+
+public class Program3 {
+		public static void main(String[] argv)
+		{		char ch = 'c';
+			int num = 88;
+			ch = 's';
+		}
+	}
